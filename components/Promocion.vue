036bb57f9aec5fdc37f3fbe67412365c073ed6ea
devrik99 <devrikoficial@gmail.com>
2023-07-13 16:23:46 -0500

    Se termino la primera version de la vista principal de promociones

---
+++ components/Promocion.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex justify-between items-start gap-5 mb-8">
+    <img src="/img/tv.png" alt="tv" class="h-full rounded" />
+    <section class="w-[240px]">
+      <p class="font-medium">Lg TV - 69% de descuento</p>
+      <span
+        >Hola soy Indra y te puedo ayudar a adquirir hasta un 69% de descuento para que lo
+        adquieras hoy mismo.</span
+      >
+    </section>
+  </div>
+</template>
+
+<script setup></script>
--- pages/promociones.vue
+++ pages/promociones.vue
@@ -1,29 +1,12 @@
 <template>
   <div class="m-[45px]">
-    <h2 class="text-[#FC2016] font-bold text-2xl my-6">Registrate para ingresar</h2>
-    <section class="flex flex-col justify-center items-center gap-5 w-[294px] mx-auto">
-      <p class="mx-auto font-medium w-full text-center">Foto de perfil</p>
-      <div class="w-[160px] h-[160px] rounded-full bg-[#D9D9D9] my-[30px]"></div>
-      <form @submit.prevent="">
-        <label for="Nombres" class="text-base font-[400]">Nombres</label>
-        <input
-          type="text"
-          class="bg-[#D9D9D9] w-full rounded p-2 mb-2"
-          placeholder="Rodrigo"
-        />
-        <label for="Apellidos" class="text-base font-[400]">Apellidos</label>
-        <input
-          type="text"
-          class="bg-[#D9D9D9] w-full rounded p-2 mb-2"
-          placeholder="Fernandez Panta"
-        />
-        <button
-          type="button"
-          class="bg-[#FFE81E] text-[#FC2016] w-full p-2 rounded-[40px] mt-[30px]"
-        >
-          Ingresar
-        </button>
-      </form>
+    <h2 class="text-[#FC2016] font-bold text-2xl my-6">Todas las promociones</h2>
+    <section class="flex flex-col justify-center items-start gap-5 mx-auto">
+      <p class="mx-auto font-medium w-full">Últimas promociones</p>
+
+      <Promocion />
+      <Promocion />
+      <Promocion />
     </section>
   </div>
 </template>
