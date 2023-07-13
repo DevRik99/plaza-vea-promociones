1ea79bfc4cb7597158c17701473e6cffc8eca3fa
devrik99 <devrikoficial@gmail.com>
2023-07-13 16:49:06 -0500

    Se termino la primera version de la vista de promocion individual

--- components/Promocion.vue
+++ components/Promocion.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-between items-start gap-5 mb-8">
+  <nuxt-link to="/promociones/1" class="flex justify-between items-start gap-5 mb-8">
     <img src="/img/tv.png" alt="tv" class="h-full rounded" />
     <section class="w-[240px]">
       <p class="font-medium">Lg TV - 69% de descuento</p>
@@ -8,7 +8,7 @@
         adquieras hoy mismo.</span
       >
     </section>
-  </div>
+  </nuxt-link>
 </template>
 
 <script setup></script>
---
+++ pages/promociones/[id].vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="m-[45px]">
+    <nuxt-link to="/promociones" class="text-[#FC2016] font-bold text-2xl my-6"
+      >Lg TV - 69% de descuento</nuxt-link
+    >
+    <img src="/img/tv-full.png" alt="TV FULL" class="my-[30px]" />
+    <div class="flex items-center">
+      <img src="/img/profile.png" alt="" />
+      <p class="font-bold text-[20px]">Indra panta</p>
+    </div>
+    <p class="mb-5">
+      Hola soy Indra y te puedo ayudar a adquirir hasta un 69% de descuento. Solo llego a
+      los Plaza Vea de San Miguel y Breña.
+    </p>
+    <p>Publicado a las 12:30AM</p>
+    <div class="flex gap-2 mb-[30px]">
+      <button
+        type="button"
+        class="bg-[#FC2016] text-white p-2 rounded-[40px] mt-[30px] text-center w-[240px] font-bold"
+      >
+        Reservar
+      </button>
+      <button
+        type="button"
+        class="bg-[#eaeaea] text-black w-[45px] p-2 px-4 rounded-[40px] mt-[30px] text-center font-medium"
+      >
+        16
+      </button>
+    </div>
+    <section>
+      <p class="text-base font-bold my-2">Comentarios</p>
+      <section class="flex mb-[30px]">
+        <img src="/img/profile.png" class="w-[48px] h-[48px] rounded-full" alt="" />
+        <div>
+          <p class="font-medium">Tom Holland</p>
+          <span
+            >Muchas gracias Indra, realmente me ayudaste con la compra de ese producto.
+            Estoy muy feliz con la nueva tele para mi casa :3</span
+          >
+        </div>
+      </section>
+      <section class="flex mb-[30px]">
+        <img src="/img/profile.png" class="w-[48px] h-[48px] rounded-full" alt="" />
+        <div>
+          <p class="font-medium">Tom Holland</p>
+          <span
+            >Muchas gracias Indra, realmente me ayudaste con la compra de ese producto.
+            Estoy muy feliz con la nueva tele para mi casa :3</span
+          >
+        </div>
+      </section>
+    </section>
+  </div>
+</template>
+
+<script setup></script>
